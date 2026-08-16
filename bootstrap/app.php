dcd5e10840ea5bc2ff6e0d2a7263b1872de1eae1
Ballon14 <akungithub1465@gmail.com>
2026-08-17 01:57:47 +0700

    fix: konflik dua instance Alpine dengan Livewire 4 matikan wire:click; pakai Alpine milik Livewire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
+
+        $middleware->appendToGroup('web', \App\Http\Middleware\PreventStaleCache::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,22 @@
 import './bootstrap';
 import './pwa/offline';
 import { connection, offlinePos, syncBanner } from './pwa/offlinePos';
-
+import { registerSW } from 'virtual:pwa-register';
 import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+registerSW({ immediate: true });
 
-Alpine.data('connection', connection);
-Alpine.data('offlinePos', offlinePos);
-Alpine.data('syncBanner', syncBanner);
+const livewireAlpine = window.Alpine;
 
-Alpine.start();
+if (livewireAlpine) {
+    window.Alpine = livewireAlpine;
+    livewireAlpine.data('connection', connection);
+    livewireAlpine.data('offlinePos', offlinePos);
+    livewireAlpine.data('syncBanner', syncBanner);
+} else {
+    window.Alpine = Alpine;
+    Alpine.data('connection', connection);
+    Alpine.data('offlinePos', offlinePos);
+    Alpine.data('syncBanner', syncBanner);
+    Alpine.start();
+}
